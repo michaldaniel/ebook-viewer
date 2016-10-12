2c8f004d485d59565b9723026964f371fb154aad
Michal Daniel <michal.p.daniel@live.com>
2016-10-12 04:05:34 +0200

    Early draft

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$("#screenshot-slider").owlCarousel({
+        navigation : false,
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true,
+        transitionStyle:"fade",
+        autoWidth:true,
+        autoHeight:true,
+        loop:true,
+        autoPlay:true
+	});
+});
+
+
+
+
