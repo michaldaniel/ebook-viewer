52c9d0f530557882b254f38c2eac68e25d930155
Michal Daniel <contact@michaldaniel.eu>
2016-12-08 21:25:27 +0100

    Merge remote-tracking branch 'xerxes2/master'
    # Conflicts:
    #	Makefile
    #	src/components/preferences_dialog.py
    #	src/components/viewer.py
    #	src/main_window.py (reverted from commit bb1fbd45cd221e2a2f00aa62ff178fcf597f3ea6)

--- src/components/viewer.py
+++ src/components/viewer.py
@@ -16,9 +16,9 @@
 import gi
 
 gi.require_version('Gtk', '3.0')
-gi.require_version('WebKit2', '4.0')
-from gi.repository import WebKit2 as WebKit
-from gi.repository import Gdk, Gtk
+gi.require_version('WebKit', '3.0')
+from gi.repository import WebKit
+
 
 class Viewer(WebKit.WebView):
     def __init__(self, window):
@@ -26,37 +26,49 @@ class Viewer(WebKit.WebView):
         Provides Webkit WebView element to display ebook content
         :param window: Main application window reference, serves as communication hub
         """
-        self.__window = window
+        WebKit.WebView.__init__(self)
 
-        self.manager = WebKit.UserContentManager.new()
-        WebKit.WebView.__init__(self, user_content_manager=self.manager)
-
-        color = self.__window.get_style_context().get_background_color(Gtk.StateType.NORMAL)
-        self.set_background_color(color)
+        # Allow transparency so we can use GTK theme as background
+        # Can be overridden by CSS background property, needs to be rgba(0,0,0,0)
+        self.set_transparent(True)
 
         # Sets WebView settings for ebook display
         # No java script etc.
+        self.set_full_content_zoom(True)
         settings = self.get_settings()
-        settings.props.enable_javascript = False
+        settings.props.enable_scripts = False
         settings.props.enable_plugins = False
         settings.props.enable_page_cache = False
-        settings.props.enable_java = False
-        settings.props.enable_html5_local_storage = False
+        settings.props.enable_java_applet = False
         try:
             settings.props.enable_webgl = False
         except AttributeError:
             pass
 
+        # Disable default menu: contains copy and reload options
+        # Reload messes with custom styling, doesn't reload CSS
+        # App is using own "copy" right click hack
+        # It will allow in future to add more options on right click
+        settings.props.enable_default_context_menu = False
+
+        settings.props.enable_html5_local_storage = False
+
         self.connect('context-menu', self.callback)
+
+        self.__window = window
 
     def load_current_chapter(self):
         """
         Loads current chapter as pointed by content porvider
         """
         file_url = self.__window.content_provider.get_chapter_file(self.__window.content_provider.current_chapter)
+        # Using WebView.load_html_string since WebView.load_uri files for some html files
+        # while load_html_string works just fine
+        # It's a bug that needs to be resolved upstream
+
         try:
             with open(file_url) as file_open:
-                self.load_html(file_open.read(), "file://" + file_url)
+                self.load_html_string(file_open.read(), "file://" + file_url)
                 print("Loaded: " + file_url)
         except IOError:
             print("Could not read: ", file_url)
@@ -65,30 +77,17 @@ class Viewer(WebKit.WebView):
         """
         Sets style to day CSS
         """
-        _file = open("/usr/share/easy-ebook-viewer/css/day.css")
-        css_str = _file.read()
-        _file.close()
-        style_sheet = WebKit.UserStyleSheet.new(css_str, WebKit.UserContentInjectedFrames.ALL_FRAMES,
-                                                WebKit.UserStyleLevel.USER, None, None)
-        self.manager.add_style_sheet(style_sheet)
+        settings = self.get_settings()
+        settings.props.user_stylesheet_uri = "file:///usr/share/easy-ebook-viewer/css/day.css"
         # TODO: Prefix location of day.css so it can be set during install
 
     def set_style_night(self):
         """
         Sets style to night CSS
         """
-        _file = open("/usr/share/easy-ebook-viewer/css/night.css")
-        css_str = _file.read()
-        _file.close()
-        style_sheet = WebKit.UserStyleSheet.new(css_str, WebKit.UserContentInjectedFrames.ALL_FRAMES,
-                                                WebKit.UserStyleLevel.USER, None, None)
-        self.manager.add_style_sheet(style_sheet)
+        settings = self.get_settings()
+        settings.props.user_stylesheet_uri = "file:///usr/share/easy-ebook-viewer/css/night.css"
         # TODO: Prefix location of night.css so it can be set during install
-
-    def update_background(self):
-        color = self.__window.get_style_context().get_background_color(Gtk.StateType.NORMAL)
-        self.set_background_color(color)
 
     def callback(self, webview, context_menu, hit_result_event, event):
         self.__window.show_menu()
-        return True
